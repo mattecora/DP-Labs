20bc2da561e28a157e47bf2344c5172917ce2f31
Matteo Corain <m96.corain@gmail.com>
2019-03-22 10:26:31 +0100

    Lab #1: exercises 2-3, first version

---
+++ lab01/ex03/ex03.c
@@ -0,0 +1,116 @@
+/******************************************************************************
+ * Lab 01 - Exercise 3                                                        *
+ * Matteo Corain - Distributed programming I - A.Y. 2018-19                   *
+ ******************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define BUF_LEN 100
+
+int main(int argc, char const *argv[])
+{
+    int s;
+    uint16_t port, n1, n2;
+    char buffer[BUF_LEN];
+    struct sockaddr_in addr;
+    struct in_addr ip;
+
+    /* Check input parameters */
+    if (argc < 3)
+    {
+        fprintf(stderr, "Not enough input parameters.\n");
+        return -1;
+    }
+
+    /* Clear the memory and set address family */
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+
+    /* Convert address and set */
+    if (inet_pton(AF_INET, argv[1], &ip) <= 0)
+    {
+        fprintf(stderr, "Not a valid IP address!\n");
+        return -1;
+    }
+    addr.sin_addr = ip;
+    printf("Address correctly set!\n");
+
+    /* Convert port and set */
+    if (sscanf(argv[2], "%hu", &port) <= 0)
+    {
+        fprintf(stderr, "Not a valid port!\n");
+        return -1;
+    }
+    addr.sin_port = htons(port);
+    printf("Port correctly set!\n");
+
+    /* Open the socket */
+    s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (s == -1)
+    {
+        fprintf(stderr, "Cannot create socket.\n");
+        return -1;
+    }
+    printf("Socket correctly opened!\n");
+
+    /* Connect the socket */
+    if (connect(s, (struct sockaddr*) &addr, sizeof(addr)) == -1)
+    {
+        fprintf(stderr, "Cannot connect socket.\n");
+        return -1;
+    }
+    printf("Socket correctly connected!\n");
+
+    while (1)
+    {
+        /* Read input */
+        printf("Insert two numbers (or quit): ");
+        fgets(buffer, BUF_LEN, stdin);
+
+        /* Quit if command received */
+        if (strncmp(buffer, "quit", strlen("quit")) == 0)
+        {
+            printf("Received quit.\n");
+            break;
+        }
+
+        /* Check input */
+        if (sscanf(buffer, "%hu %hu", &n1, &n2) < 2)
+        {
+            fprintf(stderr, "Invalid number format.\n");
+            continue;
+        }
+
+        /* Prepare and send buffer */
+        memset(buffer, 0, BUF_LEN);
+        sprintf(buffer, "%hu %hu\r\n", n1, n2);
+        if (send(s, buffer, strlen(buffer), 0) < 0)
+        {
+            fprintf(stderr, "Cannot write on socket.\n");
+            return -1;
+        }
+        
+        /* Clear and receive buffer */
+        memset(buffer, 0, BUF_LEN);
+        if (recv(s, buffer, BUF_LEN, 0) < 0)
+        {
+            fprintf(stderr, "Cannot receive from socket.\n");
+            return -1;
+        }
+
+        /* Print the answer */
+        printf("Answer: %s", buffer);
+    }
+
+    /* Close the socket */
+    close(s);
+
+    return 0;
+}
