59b90646d2598d336584c0c9212752f72e2d7880
Matteo Corain <m96.corain@gmail.com>
2019-06-19 12:16:55 +0200

    Web exam: terminate script on HTTPS redirect

--- webexam/app/session.php
+++ webexam/app/session.php
@@ -20,8 +20,10 @@
          */
         public function __construct() {
             // Enforce HTTPS usage
-            if (!isset($_SERVER["HTTPS"]))
+            if (!isset($_SERVER["HTTPS"])) {
                 header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
+                exit;
+            }
             
             // Start the session
             if (session_status() === PHP_SESSION_NONE)
